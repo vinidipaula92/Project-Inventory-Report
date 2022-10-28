19924402e91224b2a67f27f52aa634ccde025aa3
vinidipaula92 <vinidipaula92@gmail.com>
2022-10-28 15:27:06 -0300

    test: :white_check_mark: Implementaçao de teste para criação de relatório sobre o produto
    Relátorio retorna com as informações do produto e onde ele deve ser armazenado

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,17 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    product = Product(
+        1,
+        "Café",
+        "Nescafé",
+        "2020-01-01",
+        "2021-01-01",
+        "123456789",
+        "em local seco e fresco",
+    )
+    assert product.__repr__() == (
+        "O produto Café fabricado em 2020-01-01 por Nescafé com validade"
+        " até 2021-01-01 precisa ser armazenado em local seco e fresco."
+    )
