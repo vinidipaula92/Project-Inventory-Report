74903f843f841c65bc823b491e56384aaf339979
vinidipaula92 <vinidipaula92@gmail.com>
2022-10-28 15:21:49 -0300

    test: :white_check_mark: Implementação de teste para criação de produto
    Testando o __init__ da classe Product, para verificar se está sendo criado o produto corretamente

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,19 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    produto = Product(
+        1,
+        "Coca-Cola",
+        "Coca-Cola Company",
+        "2021-01-01",
+        "2021-12-31",
+        "123456789",
+        "em local fresco e seco",
+    )
+    assert produto.nome_da_empresa == "Coca-Cola Company"
+    assert produto.nome_do_produto == "Coca-Cola"
+    assert produto.data_de_fabricacao == "2021-01-01"
+    assert produto.data_de_validade == "2021-12-31"
+    assert produto.numero_de_serie == "123456789"
+    assert produto.instrucoes_de_armazenamento == "em local fresco e seco"
